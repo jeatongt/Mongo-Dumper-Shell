65e2e52b3feb1b31209252d5d983022bffbe05fb
jeatongt <jeatongt@icloud.com>
2020-02-19 14:26:32 -0500

    Completed argparse and functions

--- mongo_dumper_shell.py
+++ mongo_dumper_shell.py
@@ -21,30 +21,51 @@ def get_mongo_session():
     return MongoClient(url)['dwmops-db']
 
 
-def find_messages_by_guid_single_topic(topic_to_search, guid_to_find):
-    topic_records = db[topic_to_search].find({'headers.guid': guid_to_find})
-    records = [record for record in topic_records]
-    pprint.pprint(records)
+def search_collections(**kwargs):
+    """
+    Accepts 'topic', 'guid', and 'email'
 
-
-def find_messages_by_guid(guid_to_find):
-    list_of_collections = db.list_collection_names()
-    for collection in list_of_collections:
-        topic_records = db[collection].find({'headers.guid': guid_to_find})
-        records = [record for record in topic_records]
-        if records != []:
-            print("Messages found in collection", collection)
-            pprint.pprint(records)
+    :param kwargs:
+    :return:
+    """
+    if kwargs["topic"]:
+        if kwargs["guid"]:
+            topic_records = db[kwargs["topic"]].find({'headers.guid': kwargs["guid"]})
+        elif kwargs["email"]:
+            topic_records = db[kwargs["topic"]].find({'value.email_address': kwargs["email"]})
+        else:
+            print("  Use --email or --guid to search for specific records.  Use 'dump' to get entire collection")
             print()
+    else:
+        list_of_collections = db.list_collection_names()
+        if kwargs["guid"]:
+            for collection in list_of_collections:
+                topic_records = db[collection].find({'headers.guid': kwargs["guid"]})
+        elif kwargs["email"]:
+            for collection in list_of_collections:
+                topic_records = db[collection].find({'value.email_address': kwargs["email"]})
+        else:
+            print("  Use --email or --guid to search for specific records.  Use 'dump' to get entire collection")
             print()
 
 
 def get_list_of_collections():
+    """
+    Returns list of all collections in DB
+
+    :return:
+    """
     collections = db.list_collection_names()
     pprint.pprint(collections)
 
 
 def dump_topic(topic_name):
+    """
+    Dumps entire collection to file
+    
+    :param topic_name:
+    :return:
+    """
     topic_records = db[topic_name].find({})  # '{}' pulls all the records
     records = [record for record in topic_records]
     with open(os.path.join(os.getcwd(), topic_name), 'w') as f:
@@ -65,45 +86,47 @@ parser_search.add_argument('--email', '-e')
 parser_search.add_argument('--topic', '-t')
 parser_search.set_defaults(func=search_collections)
 
-
 while 1 == 1:
     cmd = input("> ")
     cmd_array = cmd.split()
-    if cmd_array[0] == "list":
-        get_list_of_collections()
-    elif cmd_array[0] == "dump":
-        if len(cmd_array) == 1:
-            print("Usage: > dump {collection name}")
-            break
-        else:
-            dump_topic(cmd_array[1])
-            print("Collection dumped to new file ", cmd_array[1])
-            break
-    elif cmd_array[0] == "guid":
-        if len(cmd_array) == 1:
-            print("Usage: > guid [-t topic_name] {guid}")
-        elif cmd_array[1] == "-t":
-            if cmd_array[2] == "":
-                print("Usage: > guid [-t topic_name] {guid}")
-                break
-            elif cmd_array[3] == "":
-                print("Usage: > guid [-t topic_name] {guid}")
-                break
-            else:
-                find_messages_by_guid_single_topic(cmd_array[2], cmd_array[3])
-        else:
-            if cmd_array[1] == "":
-                print("Usage: > guid [-t topic_name] {guid}")
-                break
-            else:
-                find_messages_by_guid(cmd_array[1])
-    elif cmd_array[0] == "test":
-        cre = db.CampaignResponse_Canon_12p
-        pprint.pprint(cre.find_one({'headers.guid': '82c0f8fe-4cff-11ea-9c77-0a58ac14b5c8'}))
-    elif cmd_array[0] == "exit":
-        break
-    else:
-        print("Unknown command")
+    args = parser.parse_args(cmd_array)
+    args.func(args)
 
-
-
+# while 1 == 1:
+#     cmd = input("> ")
+#     cmd_array = cmd.split()
+#     if cmd_array[0] == "list":
+#         get_list_of_collections()
+#     elif cmd_array[0] == "dump":
+#         if len(cmd_array) == 1:
+#             print("Usage: > dump {collection name}")
+#             break
+#         else:
+#             dump_topic(cmd_array[1])
+#             print("Collection dumped to new file ", cmd_array[1])
+#             break
+#     elif cmd_array[0] == "guid":
+#         if len(cmd_array) == 1:
+#             print("Usage: > guid [-t topic_name] {guid}")
+#         elif cmd_array[1] == "-t":
+#             if cmd_array[2] == "":
+#                 print("Usage: > guid [-t topic_name] {guid}")
+#                 break
+#             elif cmd_array[3] == "":
+#                 print("Usage: > guid [-t topic_name] {guid}")
+#                 break
+#             else:
+#                 find_messages_by_guid_single_topic(cmd_array[2], cmd_array[3])
+#         else:
+#             if cmd_array[1] == "":
+#                 print("Usage: > guid [-t topic_name] {guid}")
+#                 break
+#             else:
+#                 find_messages_by_guid(cmd_array[1])
+#     elif cmd_array[0] == "test":
+#         cre = db.CampaignResponse_Canon_12p
+#         pprint.pprint(cre.find_one({'headers.guid': '82c0f8fe-4cff-11ea-9c77-0a58ac14b5c8'}))
+#     elif cmd_array[0] == "exit":
+#         break
+#     else:
+#         print("Unknown command")
